52b09c9c402d685ac36263f94a3259509191245a
Ray Climie <seederwood@gmail.com>
2017-07-19 22:03:31 -0300

    Initial Commit

---
+++ swiftapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftapp
+//
+//  Created by Raymond Climie on 07/19/17.
+//  Copyright © 2017 Diva Beaner Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
