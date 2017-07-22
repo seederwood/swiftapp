9c6aa84e21ee0a2b5a020aee0f97c8333c63ce1c
Ray Climie <seederwood@gmail.com>
2017-07-21 22:29:27 -0300

    add text fields for input

--- swiftapp/ViewController.swift
+++ swiftapp/ViewController.swift
@@ -12,20 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        tapCount = tapCount + 1
-        if tapCount == 10 {
-            theLabel.text = "You Pressed me 10 times!"
-            tapCount = 0
-        }
+        
+        print(text1.text!)
+        print(text2.text!)
         
     }
     
-    @IBAction func otherButtonTapped(_ sender: Any) {
-        theLabel.text = "Buttons are cool!"
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
